a722f49035783551c33b545920bbc22c58636471
Matthew <mtthwmrls9@gmail.com>
2021-11-21 15:36:30 -0700

    Refactoring, making the UI look less offensive, and adding number tones to tone mark pinyin conversion

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
+import ExpandedDefinition from "./components/js/ExpandedDefinition";
+import WordListView from "./components/js/WordListView";
+import { Word as WordModel } from "./datamodels";
 
 const App = () => {
 
     const [text, setText] = useState('');
-    const [engText, setEngText] = useState('');
     const [words, setWords] = useState([]);
     const [selectedWord, setSelectedWord] = useState('');
     const [displayPinyin, setDisplayPinyin] = useState(true);
+    const [convertPinyin, setConvertPinyin] = useState(true);
+    const [percentComplete, setPercentComplete] = useState(0);
 
     const handleChange = (event) => {
-        console.log("Change");
         setText(event.target.value);
     } 
 
@@ -51,65 +54,66 @@ const App = () => {
                 }
             }
         }
-        return [longest_word_simplified, longest_word_traditional, longest_word_english, longest_word_pinyin];
+        return new WordModel(longest_word_simplified, longest_word_traditional, longest_word_english, longest_word_pinyin);
     }
 
     const handleSubmit = async (event) => {
         const characters = text.split('');
         setWords([]);
+        const newWords = [];
         for (let i = 0; i < characters.length; i++) {
-            const definition = await handleCharacter(characters, i);
-            if (!definition)
+            const word = await handleCharacter(characters, i);
+            if (!word)
             {
                 continue;
             }
-            const [simplified, traditional, english, pinyin] = definition;
-            if (english)
-            {                
-                setWords(words => [...words, {simplified, traditional, english, pinyin}])
-                i += simplified.length - 1;
+            if (word.english)
+            {         
+                newWords.push(word)
+                i += word.simplified.length - 1;
+                setPercentComplete(i/characters.length);
             }
         }
+        setWords(newWords);
 
     }
 
-    const handleClickWord = (obj) => {
-        setSelectedWord(obj);
+    const handleClickWord = (word) => {
+        console.log(word);
+        setSelectedWord(word);
     }
 
     const handlePinyinCheck = (event) => {
-        console.log(displayPinyin);
-        console.log(event.target.checked);
         setDisplayPinyin(event.target.checked);
     }
 
+    const handleConvertCheck = (event) => {
+        setConvertPinyin(event.target.checked);
+    }    
+
     return (
         <div>
-            <div>
+            <div className="row">
+                <h1>Enter Text:</h1>
+            </div>
+            <div className="row">
                 <textarea onChange={handleChange} style={{width: '100%'}}>
                 </textarea>
             </div>
-            <div>
+            <div className="row">
                 <button onClick={handleSubmit}>Submit</button>
-                <input type="checkbox" checked={displayPinyin} onChange={handlePinyinCheck}/>
+                <input id="pinyinCheckbox" type="checkbox" checked={displayPinyin} onChange={handlePinyinCheck}/>
+                <label for="pinyinCheckbox">Display Pinyin</label>
+                <input id="convertCheckbox" type="checkbox" checked={convertPinyin} onChange={handleConvertCheck}/>
+                <label for="convertCheckbox">Covert Pinyin (pin1yin1 -> pinyin)</label>
             </div>
-            <div>
-                <p>{words.map((element) => {
-                    if (displayPinyin)
-                    {
-                        return (<ruby className="word" onClick={() => handleClickWord(element)}>{element.simplified}<rp>(</rp><rt>{element.pinyin}</rt><rp>)</rp></ruby>)
-                    } else {
-                        return (<span className="word" onClick={() => handleClickWord(element)}>{element.simplified}</span>)
-                    }
-                })}
-                </p>
-                <br></br>
-                {selectedWord && 
-                <div>
-                    <ruby className="definition-header">{selectedWord.simplified}<rp>(</rp><rt>{selectedWord.pinyin}</rt><rp>)</rp></ruby>
-                    <span className="definition-body">{selectedWord.english}</span>
+            <div className="row">
+                <div class="word-list-column">
+                    {words.length > 0 ? <WordListView words={words} displayPinyin={displayPinyin} onClickWord={handleClickWord} convertPinyin={convertPinyin}/> : percentComplete > 0 && <h1>{Math.floor(percentComplete)}%</h1>}
                 </div>
-                }
+                <div class="expanded-definition-column">
+                    {selectedWord && <ExpandedDefinition word={selectedWord} convertPinyin={convertPinyin}/>}
+                </div>
             </div>
         </div>
     )
